48b55a9542595ba018b1a6274d74515a6ab35494
h-soon.kim <noisonnoiton@gmail.com>
2021-02-26 10:49:58 +0900

    add POST idSetting

--- src/main/java/com/htdp1/emp/empspring/EmpSpringApplication.java
+++ src/main/java/com/htdp1/emp/empspring/EmpSpringApplication.java
@@ -2,11 +2,15 @@ package com.htdp1.emp.empspring;
 
 import javax.sql.DataSource;
 
+import com.htdp1.emp.empspring.employee.model.Employee;
+import com.htdp1.emp.empspring.employee.repository.EmployeeRepository;
+
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -14,6 +18,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 import org.springframework.data.jdbc.repository.config.MyBatisJdbcConfiguration;
+import org.springframework.data.relational.core.mapping.event.BeforeSaveEvent;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -46,5 +51,24 @@ public class EmpSpringApplication {
 		sessionFactory.setConfigLocation(myBatisConfig);
 		
 		return sessionFactory.getObject();
+	}
+
+	@Autowired
+	EmployeeRepository employeeRepository;
+
+	@Bean
+	@SuppressWarnings("rawtypes")
+	public ApplicationListener<BeforeSaveEvent> idSetting() {
+	
+		return event -> {
+	
+			if (event.getEntity() instanceof Employee) {
+
+				Employee employee = (Employee) event.getEntity();
+				if (employee.getEmpNo() == null) {
+					employee.setEmpNo(employeeRepository.count() + 200000L);
+				}
+			}
+		};
 	}
 }
--- src/main/java/com/htdp1/emp/empspring/employee/model/Employee.java
+++ src/main/java/com/htdp1/emp/empspring/employee/model/Employee.java
@@ -1,9 +1,11 @@
 package com.htdp1.emp.empspring.employee.model;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceConstructor;
 import org.springframework.data.relational.core.mapping.Table;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,14 +18,28 @@ public class Employee {
 
     @Id
     private Long empNo;
-
     private String firstName;
-
     private String lastName;
-
     private String birthDate;
-
     private String hireDate;
-
     private String gender;
+
+    @Builder
+    @PersistenceConstructor
+    public Employee(Long empNo, String firstName, String lastName, String birthDate, String hireDate, String gender) {
+        this.empNo = empNo;
+        this.firstName = firstName;
+    	this.lastName = lastName;
+    	this.birthDate = birthDate;
+        this.hireDate = hireDate;
+        this.gender = gender;
+    }
+
+    public Employee(String firstName, String lastName, String birthDate, String hireDate, String gender) {
+        this.firstName = firstName;
+    	this.lastName = lastName;
+    	this.birthDate = birthDate;
+        this.hireDate = hireDate;
+        this.gender = gender;
+    }
 }
