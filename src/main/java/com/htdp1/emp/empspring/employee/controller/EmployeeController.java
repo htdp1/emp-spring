fbdf3c6014ba052f7e3f45449581c14b5dd4cbe3
jaemyeong.lee <robin022@naver.com>
2021-03-02 16:10:01 +0900

    log.debug 추

--- src/main/java/com/htdp1/emp/empspring/employee/controller/EmployeeController.java
+++ src/main/java/com/htdp1/emp/empspring/employee/controller/EmployeeController.java
@@ -2,10 +2,6 @@ package com.htdp1.emp.empspring.employee.controller;
 
 import java.util.Map;
 
-import com.htdp1.emp.empspring.employee.model.Employee;
-import com.htdp1.emp.empspring.employee.service.EmployeeService;
-import com.htdp1.emp.empspring.rest.service.RestService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +12,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.htdp1.emp.empspring.employee.model.Employee;
+import com.htdp1.emp.empspring.employee.service.EmployeeService;
+import com.htdp1.emp.empspring.rest.service.RestService;
+
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/v1")
+@Slf4j
 public class EmployeeController {
 
     @Autowired
@@ -29,21 +32,29 @@ public class EmployeeController {
     @GetMapping(value = "/employees/{empNo}")
     @ResponseBody
     public Employee selectEmployee(@PathVariable long empNo) throws Exception {
+    	log.debug("selectEmployee");
+    	
         return employeeService.selectEmployee(empNo);
     }
 
     @PostMapping(value = "/employees")
     public int insertEmployee(@RequestBody Employee employee) throws Exception {
+    	log.debug("insertEmployee");
+    	
         return employeeService.insertEmployee(employee);
     }
 
     @GetMapping(value = "/template/employees/{empNo}")
     @ResponseBody
     public Map<String, Object> getEmployee(@PathVariable long empNo) throws Exception {
+    	log.debug("getEmployee");
+    	
         return getEmployee();
     }
 
     private Map<String, Object> getEmployee() {
+    	log.debug("getEmployee restService");
+    	
         return restService.get("http://localhost:8080" + "/v1/employees/10001", HttpHeaders.EMPTY).getBody();
     }
 }
